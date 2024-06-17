a27920f288c5c26cffe54b0042a90902b5203c7f
MRzeczkowski <mateusz.rzeczkowski96@gmail.com>
2024-06-17 23:11:01 +0200

    some swagger and razor stuff

--- UniversitySystem/UniversitySystem/Program.cs
+++ UniversitySystem/UniversitySystem/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using UniversitySystem.Context;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,16 +13,24 @@ builder.Services.AddDbContext<UniversityContext>(options =>
     options.UseLazyLoadingProxies();
 });
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages()
+    .AddRazorRuntimeCompilation();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "University system", Version = "v1" });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
