71efb7135aaf93f724a91e92283528f8d2619ea9
MRzeczkowski <mateusz.rzeczkowski96@gmail.com>
2024-06-21 18:34:42 +0200

    join

---
+++ UniversitySystem/UniversitySystem/Migrations/20240621163214_RemoveUselessColumnsAndJoinedPersonWithAspNetUser.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UniversitySystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveUselessColumnsAndJoinedPersonWithAspNetUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PersonId",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_PersonId",
+                table: "AspNetUsers",
+                column: "PersonId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Persons_PersonId",
+                table: "AspNetUsers",
+                column: "PersonId",
+                principalSchema: "Administration",
+                principalTable: "Persons",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Persons_PersonId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_PersonId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "PersonId",
+                table: "AspNetUsers");
+        }
+    }
+}
